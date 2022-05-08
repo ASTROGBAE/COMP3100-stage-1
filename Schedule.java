ca1a8b9c0d5ed784aa6db1a4bf77b9050ed3c6c6
ASTROGBAE <gabebrett@iinet.net.au>
2022-05-08 14:22:34 +1000

    attempted to add methods for different fit methods, not implemented in overall algorithm yet

--- Schedule.java
+++ Schedule.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
@@ -60,7 +63,8 @@ public class Schedule {
             if (_server.getNodeType() == Node.ELEMENT_NODE) { // check if element type, n(eed to cast before getting
                                                               // element attribute
                 String type = ((Element) _server).getAttribute("type"); // get attribute "type"
-                int current_core = Integer.parseInt(((Element) _server).getAttribute("cores")); // get current core to compare to previous
+                int current_core = Integer.parseInt(((Element) _server).getAttribute("cores")); // get current core to
+                                                                                                // compare to previous
                 if (current_core > prev_core) { // if new biggest server type (do not change if the same)
                     largest = type;
                     prev_core = current_core;
@@ -69,5 +73,93 @@ public class Schedule {
         }
         xmlRead = true; // now can call other objects!
         return true; // done! all read
+    }
+
+    // methods for stage 2...
+    // getCapable: list of servers from DATA return
+    public Server getNextServer(String getCapable, int reqCores) {
+        ArrayList<Server> servers = new ArrayList<Server>(); // list to store servers in for processing
+        Scanner scanner = new Scanner(getCapable); // get scanner to read server list line by line
+        while (scanner.hasNextLine()) { // read in servers line by line, add to server arraylist
+            Server s = new Server(scanner.nextLine());
+            if (s.isValid()) { // read in new server, if it is valid
+                servers.add(s);
+            }
+        }
+        scanner.close(); // finish scanning in server list form capable return
+        if (!servers.isEmpty()) { // if servers added, continue
+            getMethod(servers, reqCores, "");
+        }
+        return null; // is servers is empty
+    }
+
+    private Server getMethod(ArrayList<Server> _servers, int reqCores, String method) {
+        if (_servers != null && !_servers.isEmpty() && method != null && !method.isEmpty()) {
+            _servers.sort((Server a, Server b) -> { // before analysis, sort servers by core size
+                return a.getCores() - b.getCores(); // TODO should we do this or will it screw up the algorithm??
+            });
+            if (method.equals("FC")) { // first capable method, return the first server that is capable
+                return _servers.get(0);
+            } else if (method.equals("FF")) { // first fit
+                for (Server s : _servers) {
+                    if (serverReady(s)) {
+                        return s; // return first fit, if it exists
+                    }
+                }
+                // if no target found from above, simply return first capable TODO is this
+                // right?
+                return _servers.get(0);
+            }
+
+            else if (method.equals("BF") || method.equals("WF")) { // if best fit or worst fit
+                // (similar algorithms)
+                Map<Server, Integer> fitServers = new LinkedHashMap<Server, Integer>(); // map fitness values to servers
+                // fitness value: cores of server-core requirement of job
+                for (Server s : _servers) { // populate fitness map
+                    fitServers.put((Server) s, (int) (s.getCores() - reqCores));
+                }
+                // when fitness values calculated, find best choice
+                Server target = null; // server to return, not null if a server meets "BF" criteria
+                for (Server s : _servers) { // find target server
+                    if (serverReady(s)) {
+                        if (target == null) { // if no target found, choose first viable option
+                            target = s;
+                        } else { // target not null, choose s if highest store
+                            if (fitnessComparison(fitServers.get(s), fitServers.get(target), method) == 1) {
+                                target = s; // choose new
+                            }
+                        }
+                    }
+                }
+                if (target == null) { // if no server meets above criteria
+                    for (Server s : _servers) { // find any server, most valid fitness
+                        if (target == null) { // first runthough, get highest
+                            target = s;
+                        } else { // target not null, valid comparison method
+                            if (fitnessComparison(fitServers.get(s), fitServers.get(target), method) == 1) {
+                                target = s; // choose new
+                            }
+                        }
+                    }
+                }
+                return target;
+            }
+        }
+        return null; // invalid input
+    }
+
+    // satisfy first condition of FF: state must be inactive or active second
+    // condition: has sufficient resources (covered by capable command?)
+    private Boolean serverReady(Server s) {
+        return s.getState().equals("active") || s.getState().equals("inactive");
+    }
+
+    // if BF, return '1' if a is bigger. if WF, return '1' if a is smaller.
+    private int fitnessComparison(int aFitness, int bFitness, String method) {
+        if (method.equals("BF")) {
+            return aFitness - bFitness;
+        } else { // == WF
+            return bFitness - aFitness;
+        }
     }
 }
